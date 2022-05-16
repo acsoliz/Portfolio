83da1a4668a9668f99d4f4de591c58ec064053ba
acsoliz <acsoliz@outlook.com>
2022-05-17 00:24:48 +0200

    actualizacion del link de CV en NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -115,7 +115,7 @@ const Navbar = () => {
 					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
 						<a
 							className="flex justify-between items-center w-full text-gray-300 "
-							href="https://drive.google.com/file/d/1xZgelJ4IHfoHFjA64uj2CTxgyxfsvOER/view?usp=sharing"
+							href="https://drive.google.com/file/d/1KpAZTxvvLzDlb0QpwqVzyvPyY_6R8Z3l/view?usp=sharing"
 						>
 							curriculum <BsFillPersonLinesFill size={30} />
 						</a>
