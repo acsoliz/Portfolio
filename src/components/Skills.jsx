d2a92bedd9dcc7a31315f37928d614683b0e4fef
acsoliz <acsoliz@outlook.com>
2024-06-06 16:25:49 +0200

    fixing height in skills item container

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,9 +14,27 @@ import Aws from '../assets/aws.png';
 import TypeScript from '../assets/typescript.png';
 import Sveltekit from '../assets/svelte.png';
 import Azure from '../assets/azure.png';
+import Supabase from '../assets/supabase.png';
 
 
 const Skills = () => {
+	const skills = [
+		{ name: 'HTML', icon: HTML },
+		{ name: 'CSS', icon: CSS },
+		{ name: 'JavaScript', icon: JavaScript },
+		{ name: 'React', icon: ReactImg },
+		{ name: 'GitHub', icon: GitHub },
+		{ name: 'Git', icon: Git },
+		{ name: 'Node.JS', icon: Node },
+		{ name: 'PostgresSql', icon: PostgresSql },
+		{ name: 'Redux', icon: Redux },
+		{ name: 'Aws', icon: Aws },
+		{ name: 'Tailwind', icon: Tailwind },
+		{ name: 'TypeScript', icon: TypeScript },
+		{ name: 'Azure', icon: Azure },
+		{ name: 'Supabase', icon: Supabase },
+		{ name: 'SvelteKit', icon: Sveltekit },
+	];
 	return (
 		<div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
 			{/* Container */}
@@ -25,63 +43,18 @@ const Skills = () => {
 					<p className="text-4xl font-bold inline border-b-4 border-pink-600 ">Skills</p>
 					<p className="py-4">// these are the technologies I've worked with</p>
 				</div>
-				<div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-					<div className="shadow-sm shadow-[$040c16] hover:scale-110 duration-500">
-						<img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
-						<p className="my-4">HTML</p>
-					</div>
-					<div className="shadow-sm shadow-[$040c16] hover:scale-110 duration-500">
-						<img className="w-20 mx-auto" src={CSS} alt="HTML icon" />
-						<p className="my-4">CSS</p>
-					</div>
-					<div className="shadow-sm shadow-[$040c16] hover:scale-110 duration-500">
-						<img className="w-20 mx-auto" src={JavaScript} alt="HTML icon" />
-						<p className="my-4">JAVASCRIPT</p>
-					</div>
-					<div className="shadow-sm shadow-[$040c16] hover:scale-110 duration-500">
-						<img className="w-20 mx-auto" src={ReactImg} alt="HTML icon" />
-						<p className="my-4">REACT JS</p>
-					</div>
-					<div className="shadow-sm shadow-[$040c16] hover:scale-110 duration-500">
-						<img className="w-20 mx-auto" src={GitHub} alt="HTML icon" />
-						<p className="my-4">GITHUB</p>
-					</div>
-					<div className="shadow-sm shadow-[$040c16] hover:scale-110 duration-500">
-						<img className="w-20 mx-auto" src={Git} alt="HTML icon" />
-						<p className="my-4">GIT</p>
-					</div>
-					<div className="shadow-sm shadow-[$040c16] hover:scale-110 duration-500">
-						<img className="w-20 mx-auto" src={Node} alt="HTML icon" />
-						<p className="my-4">NODE JS</p>
-					</div>
-					<div className="shadow-sm shadow-[$040c16] hover:scale-110 duration-500">
-						<img className="w-20 mx-auto" src={PostgresSql} alt="HTML icon" />
-						<p className="my-4">POSTGRES SQL</p>
-					</div>
-					<div className="shadow-sm shadow-[$040c16] hover:scale-110 duration-500">
-						<img className="w-20 mx-auto" src={Redux} alt="HTML icon" />
-						<p className="my-4">REDUX</p>
-					</div>
-					<div className="shadow-sm shadow-[$040c16] hover:scale-110 duration-500">
-						<img className="w-20 mx-auto" src={Aws} alt="HTML icon" />
-						<p className="my-4">AWS</p>
-					</div>
-					<div className="shadow-sm shadow-[$040c16] hover:scale-110 duration-500">
-						<img className="w-20 mx-auto" src={Tailwind} alt="HTML icon" />
-						<p className="my-4">Tailwind</p>
-					</div>
-					<div className="shadow-sm shadow-[$040c16] hover:scale-110 duration-500">
-						<img className="w-20 mx-auto" src={TypeScript} alt="HTML icon" />
-						<p className="my-4">TypeScript</p>
-					</div>
-					<div className="shadow-sm shadow-[$040c16] hover:scale-110 duration-500">
-						<img className="w-20 mx-auto" src={Azure} alt="HTML icon" />
-						<p className="my-4">Azure</p>
-					</div>
-					<div className="shadow-sm shadow-[$040c16] hover:scale-110 duration-500">
-						<img className="w-20 mx-auto" src={Sveltekit} alt="HTML icon" />
-						<p className="my-4">SvelteKit</p>
-					</div>
+
+				<div className="w-full grid grid-cols-5 gap-5 text-center py-8">
+
+					{skills.map((skill, index) => (
+						<div key={index} className="shadow-sm shadow-[$040c16] hover:scale-110 duration-500 flex flex-col " >
+
+							<img className="w-20 h-20 mx-auto" src={skill.icon} alt={`${skill.name} icon`} />
+
+							<p className="my-4">{skill.name}</p>
+
+						</div>
+					))}
 				</div>
 			</div>
 		</div>
